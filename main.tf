5c2c6425d02d5431a9fa531bb596189a501fa352
Ram Vellanki <vellanki.chandra@gmail.com>
2021-08-15 17:32:50 +0530

    added kubernetes cluster terraform

--- main.tf
+++ main.tf
@@ -5,6 +5,34 @@ terraform {
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
+
+  required_providers {
+    azurerm = {
+      source   = "hashicorp/azurerm"
+      version = "=2.49.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name  = "learnk8scluster"
+  dns_prefix = "learnk8scluster"
+  resource_group_name = "implementing_microservices"
+  location = "southindia"
+
+  default_node_pool {
+    name= "default"
+    node_count = "2"
+    vm_size = "standard_d2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
 # Network configuration
