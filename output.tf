c4559a761c384e02dba3e58889f910cfed5cb975
Ram Vellanki <vellanki.chandra@gmail.com>
2021-08-16 19:24:26 +0530

    removed sensitive stuff from output

--- output.tf
+++ output.tf
@@ -3,19 +3,3 @@ resource "local_file" "kubeconfig" {
   filename     = "kubeconfig"
   content      = azurerm_kubernetes_cluster.cluster.kube_config_raw
 }
-
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
-}
-
-output "client_key" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_key
-}
-
-output "cluster_ca_certificate" {
-    value = azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate
-}
-
-output "kubernetes_cluster_endpoint"{
-    value = azurerm_kubernetes_cluster.cluster.kube_config.0.host
-}
