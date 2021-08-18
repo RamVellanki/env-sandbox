7f13bbb8c5a1098e745e4ac977e180bc1f4b39c7
Ram Vellanki <vellanki.chandra@gmail.com>
2021-08-18 10:37:31 +0530

    referred to modularised k8s and argocd

--- main.tf
+++ main.tf
@@ -19,30 +19,17 @@ provider "azurerm" {
 }
 
 # Kubernetes Configuration
-
-resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = "learnk8scluster"
-  dns_prefix          = "learnk8scluster"
-  resource_group_name = "implementing_microservices"
-  location            = "southindia"
-
-  default_node_pool {
-    name       = "default"
-    node_count = "2"
-    vm_size    = "standard_d2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
+module "azure-kubernetes" {
+  source = "github.com/RamVellanki/module-azure-kubernetes"
 }
+
 
 # Gitops Configuration
 module "argo-cd-server" {
   source = "github.com/RamVellanki/module-argocd"
 
-  kubernetes_cluster_endpoint = azurerm_kubernetes_cluster.cluster.kube_config.0.host
-  client_certificate     = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
-  client_key             = azurerm_kubernetes_cluster.cluster.kube_config.0.client_key
-  cluster_ca_certificate = azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate
+  kubernetes_cluster_endpoint = module.azure-kubernetes.kubernetes_cluster_endpoint
+  client_certificate          = module.azure-kubernetes.client_certificate
+  client_key                  = module.azure-kubernetes.client_key
+  cluster_ca_certificate      = module.azure-kubernetes.cluster_ca_certificate
 }
